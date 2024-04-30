12859731e522c2c161f7a26831f4135a38f33de0
bukhalov1 <buhalov.alexei@yandex.ru>
2024-04-30 22:11:16 +0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log("Express is here")
-
-
+console.log("app.js is started...")
 
 const express = require("express");
 const app = express();
@@ -13,11 +11,24 @@ app.use((req, res, next) => {
     next(); // Передаем управление следующему middleware
   });
 
+// load static
 app.use(express.static('public'));
 
+// load site
 app.get('/',(req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+
+app.use(express.json()); // Для парсинга JSON
+
+// Обработка POST-запроса
+app.post('/test', (req, res) => {
+    console.log('Получены данные:', req.body);
+    res.status(200).json({ message: "Данные успешно получены", yourData: req.body });
+});
+
+
 
 // Запуск сервера
 app.listen(PORT, () => {
