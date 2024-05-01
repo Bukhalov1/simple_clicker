2cf40d71bdaa05cbafc011aeea3275b9b174409e
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-01 12:13:52 +0300

    update

--- User.js
+++ User.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // connect to db
-mongoose.connect('mongodb://localhost:27017/gameDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/gameDB')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err)
 );
