f9faebeffd81f070ae5f59d077b825e806e85bc4
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-01 15:09:25 +0300

    update

--- app.js
+++ app.js
@@ -100,6 +100,9 @@ app.post('/send-user-data', (req, res) => {
     console.log('Got user data:', req.body);
     res.status(200).json({ message: "Succses in getting user data", yourData: req.body });
 
+    console.log("\r\n check:")
+    console.log(checkUserExistsById(req.body.id))
+
     if(checkUserExistsById(req.body.id) == 0){
         // add new user to db
         console.log('adding new player')
