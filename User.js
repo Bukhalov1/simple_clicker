b9752c212ea39eb4945c9ac9828702a717d8cd20
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-01 14:03:46 +0300

    update

--- User.js
+++ User.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 // Определение схемы 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: false },
-    firstName: { type: Number, required: false },
+    firstName: { type: String, required: false },
     wallet: { type: String, required: false },
-    score: { type: String, required: false },
+    score: { type: Number, required: false },
     createdAt: { type: Date, default: Date.now }
 });
 
