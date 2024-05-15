3f9b6d6e7f8ae50e6152f13bca186cb50a0ecc14
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-16 00:14:43 +0300

    update

--- public/js/ton-connect.js
+++ public/js/ton-connect.js
@@ -1,7 +1,4 @@
-// console.log(11000);
-
-// import TonConnectUI from '@tonconnect/ui';
-// const tonConnectUI = new TonConnectUI({ //connect application
-//     manifestUrl: 'https://<YOUR_APP_URL>/tonconnect-manifest.json',
-//     buttonRootId: '<YOUR_CONNECT_BUTTON_ANCHOR_ID>'
-// });
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://webcrafters.ru/tonconnect-manifest.json',
+    buttonRootId: 'ton-connect-ui'
+});
