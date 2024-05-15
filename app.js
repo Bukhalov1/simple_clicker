1c622abbf721cde1a200fbc96ebec5d62b11ecc0
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-16 00:27:11 +0300

    update

--- app.js
+++ app.js
@@ -133,23 +133,6 @@ app.get('/users/sorted-by-score', async (req, res) => {
   });
 
 
-//   const tonConnectOptions = {
-//     manifestUrl: 'https://webcrafters.com/manifest.json',  // Замените на ваш реальный URL манифеста
-//     walletSelection: {
-//         include: [
-//             'tonkeeper',
-//             'tonhub',
-//             'tonwallet',
-//             // добавьте другие кошельки по мере необходимости
-//         ]
-//     }
-// };
-
-// module.exports = {
-//     tonConnectOptions
-// };
-
-
 
 // Запуск сервера
 app.listen(PORT, () => {
