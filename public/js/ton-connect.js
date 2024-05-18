d89f3b3415818ba45372596dfd65e65ff89d1836
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-19 00:39:38 +0300

    update

--- public/js/ton-connect.js
+++ public/js/ton-connect.js
@@ -1,4 +1,30 @@
-const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({                  // ТУТ БЫЛА ПРОБЛЕМА!!!!!
     manifestUrl: 'https://webcrafters.ru/tonconnect-manifest.json',
     buttonRootId: 'ton-connect-ui'
 });
+
+// console.log(tonConnectUI);
+
+// // tonConnectUI.render(tonConnectUIContainer);
+
+// // Обработчик события подключения кошелька
+// tonConnectUI.on('connect', (wallet) => {
+//     const address = wallet.address;
+//     console.log('Кошелек подключен, адрес:', address);
+
+//     // Можете сохранить адрес в состоянии или показать пользователю
+//     const addressContainer = document.createElement('div');
+//     addressContainer.textContent = `Кошелек подключен: ${address}`;
+//     document.body.appendChild(addressContainer);
+// });
+
+// // Обработчик события отключения кошелька
+// tonConnectUI.on('disconnect', () => {
+//     console.log('Кошелек отключен');
+
+//     // Можете очистить состояние или уведомить пользователя
+// });
+
+
+
+
