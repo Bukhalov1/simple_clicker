42795a5ef1e505869ff98bb5cdede9c47686def6
bukhalov1 <buhalov.alexei@yandex.ru>
2024-05-19 23:42:15 +0300

    update

--- public/js/ton-connect.js
+++ public/js/ton-connect.js
@@ -1,29 +1,52 @@
+
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({                  // ТУТ БЫЛА ПРОБЛЕМА!!!!!
     manifestUrl: 'https://webcrafters.ru/tonconnect-manifest.json',
     buttonRootId: 'ton-connect-ui'
 });
 
-// console.log(tonConnectUI);
+const tonweb = new TonWeb();
+const { Address } = tonweb;
 
-// // tonConnectUI.render(tonConnectUIContainer);
+// user-friendly address
+function convertAddress(internalAddress) {
+    const address = new Address(internalAddress);
+    const friendlyAddress = address.toString(true, true, false); // true, true, false - UQ; true, true, true - EQ
+    return friendlyAddress;
+}
 
-// // Обработчик события подключения кошелька
-// tonConnectUI.on('connect', (wallet) => {
-//     const address = wallet.address;
-//     console.log('Кошелек подключен, адрес:', address);
+// Функция для получения баланса по адресу
+async function getBalance(address) {
+    try {
+        const balance = await tonweb.provider.getBalance(address);
+        // Баланс возвращается в нано-TON, поэтому делим на 1e9, чтобы получить TON
+        const balanceInTON = balance / 1e9;
+        return balanceInTON.toFixed(2);
+    } catch (error) {
+        console.error(`Error in getting balance: ${error}`);
+        throw error;
+    }
+}
 
-//     // Можете сохранить адрес в состоянии или показать пользователю
-//     const addressContainer = document.createElement('div');
-//     addressContainer.textContent = `Кошелек подключен: ${address}`;
-//     document.body.appendChild(addressContainer);
-// });
 
-// // Обработчик события отключения кошелька
-// tonConnectUI.on('disconnect', () => {
-//     console.log('Кошелек отключен');
+document.addEventListener('DOMContentLoaded', () => {
+    const connectButton = document.getElementById('profile_btn');
+    connectButton.addEventListener('click', () => {
+        const internalAddress = tonConnectUI.wallet.account.address;
+        const friendlyAddress = convertAddress(internalAddress);
+        console.log('Wallet connected, friendlyAddress:', friendlyAddress);
+        console.log('Wallet connected, internalAddress:', internalAddress);
+        // const addressContainer = document.getElementById('your-address');
+        // addressContainer.textContent = friendlyAddress;
 
-//     // Можете очистить состояние или уведомить пользователя
-// });
+        getBalance(friendlyAddress).then(balance => {
+            console.log(`Balance: ${balance} TON`);
+            const balanceContainer = document.getElementById('ton-balance');
+            balanceContainer.textContent = `${balance} TON`;
+        }).catch(error => {
+            console.error(error);
+        });
+    });
+});
 
 
 
